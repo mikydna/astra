d3669a76aa294d16d14d0ce09daa74a875ac4be0
Andy Kim <mikydna@yahoo.com>
2016-09-04 15:33:31 -0400

    tool: integrate new ws handler

--- cmd/astra-standalone.go
+++ cmd/astra-standalone.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"time"
@@ -10,7 +9,7 @@ import (
 import (
 	"github.com/facebookgo/httpdown"
 	"github.com/mikydna/astra"
-	"golang.org/x/net/websocket"
+	"github.com/mikydna/astra/websocket"
 )
 
 var (
@@ -20,101 +19,8 @@ var (
 	}
 )
 
-type ProcessedFrame struct {
-	Index  int
-	Width  int
-	Height int
-	Buffer []int16
-}
-
-func rle(buffer []int16) []int {
-	rle := []int{}
-	count := 1
-	curr := buffer[0]
-
-	rle = append(rle, int(curr))
-	for i := 1; i < len(buffer); i++ {
-		if buffer[i] == curr {
-			count += 1
-		} else {
-			rle = append(rle, int(count), int(buffer[i]))
-
-			curr = buffer[i]
-			count = 1
-		}
-	}
-
-	rle = append(rle, int(count))
-
-	return rle
-}
-
-func downsample(frame astra.CameraDepthFrame, factor uint8) []int16 {
-	result := []int16{}
-	for r := 0; r < int(frame.Height); r += 2 {
-		for c := 0; c < int(frame.Width); c += 2 {
-			offset := r*int(frame.Width) + c
-			result = append(result, frame.Buffer[offset])
-		}
-	}
-
-	return result
-}
-
-func BroadcastDepthFrames(edge *astra.Edge) http.HandlerFunc {
-	subscribers := []chan []int{}
-
-	// consume edge frames
-	go func() {
-		for depthFrame := range edge.Depth {
-			for _, subscriber := range subscribers {
-				select {
-				case subscriber <- rle(downsample(depthFrame, 2)):
-				default:
-				}
-			}
-		}
-	}()
-
-	broadcast := func(conn *websocket.Conn) {
-		log.Println("CONN MADE")
-
-		listen := make(chan []int)
-		subscribers = append(subscribers, listen)
-		at := len(subscribers) - 1
-
-		for frame := range listen {
-			temp := struct {
-				Frame  int   `json:"frame"`
-				Buffer []int `json:"data"`
-			}{
-				Frame:  -1,
-				Buffer: frame,
-			}
-
-			b, err := json.Marshal(temp)
-			if err != nil {
-				break
-			}
-
-			if _, err := conn.Write(b); err != nil {
-				subscribers = append(subscribers[:at], subscribers[at:]...)
-				break
-			}
-		}
-
-		log.Println("CONN EXIT")
-	}
-
-	return websocket.Handler(broadcast).ServeHTTP
-}
-
 func main() {
 	done := make(chan bool)
-	// go func() {
-	//  <-time.After(100 * time.Second)
-	//  done <- true
-	// }()
 
 	// initialize cameras
 	cameras := []string{"device/default"}
@@ -128,7 +34,7 @@ func main() {
 	mux := http.NewServeMux()
 	server := &http.Server{Addr: ":9091", Handler: mux}
 
-	mux.Handle("/", BroadcastDepthFrames(edge))
+	mux.Handle("/depth", websocket.BroadcastFrames(edge, websocket.Downsample(8)))
 
 	graceful, err := httpConf.ListenAndServe(server)
 	if err != nil {
